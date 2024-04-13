2236109d799f1859d3828f382ce21d8704046565
axiu <abawor200@gmail.com>
2024-04-13 06:39:42 +0100

    Added random dice number generator and Roll button

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,30 @@
 import React from "react"
-import Die from "./Components/Die"
+import Die from "../Components/Die"
 
-/**
- * Challenge:
- * 
- * - Create a Die component that takes a `value` prop
- * - Render 10 instances of the Die component (manually)
- *      - Provide a number between 1-6 for the value on each
- *        for now
- * - Style the <main> and <Die> components 
- *   to look like they do in the slide
- *      - Hints: Create a container to hold the 10 instances
- *        of the Die component, and use CSS Grid to lay them
- *        out evenly in 2 rows of 5 columns
- *      - Use flexbox on main to center the dice container
- *        in the center of the page
- */
-
-function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
 export default function App() {
+
+    const [dice, setDice] = React.useState(allNewDice())
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+    
+    function rollDice() {
+        setDice(allNewDice())
+    }
+    
+    const diceElements = dice.map(die => <Die value={die} />)
+    
     return (
         <main>
-            <container className="dice-container">
-                <Die value={randomIntFromInterval(1, 6)} />
-                <Die value={randomIntFromInterval(1, 6)} />
-                <Die value={randomIntFromInterval(1, 6)} />
-                <Die value={randomIntFromInterval(1, 6)} />
-                <Die value={randomIntFromInterval(1, 6)} />
-                <Die value={randomIntFromInterval(1, 6)} />
-                <Die value={randomIntFromInterval(1, 6)} />
-                <Die value={randomIntFromInterval(1, 6)} />
-                <Die value={randomIntFromInterval(1, 6)} />
-                <Die value={randomIntFromInterval(1, 6)} />
-            </container>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
         </main>
     )
 }
