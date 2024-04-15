48faf8000fc164f1bfbf6ab80ea021ae14ff5b03
axiu <abawor200@gmail.com>
2024-04-15 13:46:12 +0100

    Added confetti effect when game is won

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Confetti from "react-confetti"
 import Die from "../Components/Die"
 import {nanoid} from "nanoid"
 
@@ -63,6 +64,7 @@ export default function App() {
     
     return (
         <main>
+            {tenzies ? <Confetti /> : ""}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">
                 Roll until all dice are the same. Click each die to freeze
