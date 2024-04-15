fc1ebb75706df2d79b303f808011523766e2ae5f
axiu <abawor200@gmail.com>
2024-04-15 13:17:30 +0100

    Setup rollDice function to roll only unheld dices

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,36 @@ export default function App() {
 
     const [dice, setDice] = React.useState(allNewDice())
     
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
+    
     function allNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
-            newDice.push({
-                value: (Math.ceil(Math.random() * 6)),
-                isHeld: false,
-                id: nanoid()
-            })
+            newDice.push(generateNewDie())
         }
         return newDice
     }
     
     function holdDice(id) {
-        setDice(dice.map(die => ({
-            ...die,
-            isHeld: die.id === id ? !die.isHeld : die.isHeld
-        })
-    ))}
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ? 
+                {...die, isHeld: !die.isHeld} :
+                die
+        }))
+    }
 
     function rollDice() {
-        setDice(allNewDice())
+        setDice(oldDice => oldDice.map(die => {
+            return die.isHeld ? 
+                die :
+                generateNewDie()
+        }))
     }
     
     let diceElements = dice.map(die =>
@@ -41,6 +50,11 @@ export default function App() {
     
     return (
         <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">
+                Roll until all dice are the same. Click each die to freeze
+                 it at its current value between rolls.
+            </p>
             <div className="dice-container">
                 {diceElements}
             </div>
