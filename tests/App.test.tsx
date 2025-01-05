6b3b9f10ab55873dbbfaffbccb34eb43eb263bc6
abawor <abawor200@gmail.com>
2025-01-05 14:53:48 +0000

    Test that 10 dies are rendered

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -27,6 +27,11 @@ describe('App', () => {
         expect(updatedCurrentScore).toBeInTheDocument()
     })
 
+    it('Render 10 dice elements', () => {
+        const diceElements = screen.getAllByTestId("die")
+        expect(diceElements.length).toBe(10)
+    })
+
     it('Update Record to 5 after 4 rolls if tenzies is true', async () => {
         vi.spyOn(global.Math, "random").mockReturnValue(0.1)
         const button = screen.getByRole("button", { name: /Roll/i })
